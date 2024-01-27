3b9d8d0c828801e61afdcfe78338e18844f1722a
Shael more <89303457+Shael-More@users.noreply.github.com>
2024-01-27 23:46:30 +0530

    minor changesto file

--- main.js
+++ main.js
@@ -2,29 +2,29 @@ const img = document.querySelector('.main-img img');
 const toggleBtn = document.querySelector('.toggle');
 const toggleText = document.querySelectorAll('.toggle-text');
 
-
 // toggle variable
 let bool = true;
 
 toggleBtn.addEventListener('click', () => {
-    if(bool === true) {
-        img.src = "./cup-light.png";
+  if (bool === true) {
+    img.src = './cup-light.png';
 
-        toggleText.forEach(text => {
-            text.innerText = "White";
-        });
-        bool = false;
-    } else {
-         img.src = "./cup-dark.png";
+    toggleText.forEach((text) => {
+      text.innerText = 'White';
+    });
 
-        toggleText.forEach(text => {
-            text.innerText = "Blvck";
-        });
-        bool = true;
-    }
+    bool = false;
+  } else {
+    img.src = './cup-dark.png';
 
-    document.body.classList.toggle('light-theme');
+    toggleText.forEach((text) => {
+      text.innerText = 'Blvck';
+    });
+    
+    bool = true;
+  }
 
-    toggleBtn.firstElementChild.classList.toggle('fa-sun');
+  document.body.classList.toggle('light-theme');
 
+  toggleBtn.firstElementChild.classList.toggle('fa-sun');
 });
